e03bcd4f3a7bd8ddc2dd1029f1034fd3f3a1fc5d
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-03-17 00:44:00 -0400

    add versioneer

--- binderbot/__init__.py
+++ binderbot/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """Ryan Abernathey"""
 __email__ = 'ryan.abernathey@gmail.com'
 __version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
