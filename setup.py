5cc3d25e2980478ea97e0aa1c3f9bb0b4ec96319
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-03-17 00:58:43 -0400

    update reqs again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'aiohttp', 'yarl', 'structlog', 'nbformat']
+requirements = ['Click>=7.0', 'aiohttp', 'yarl', 'structlog', 'nbformat', 'nbconvert']
 
 setup_requirements = ['pytest-runner', ]
 
